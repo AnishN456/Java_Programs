0e36ed658ba9b7c16607a3dd366cc7ddd1d1233b
Anish N <Anish N@DESKTOP-D3DUG4T>
2022-05-24 23:10:23 +0530

    Java Operators Programs

---
+++ Operators/src/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		System.out.println("Addition of two numbers: "+(a+b));
+		System.out.println("Subtraction of two nmbers: "+(b-a));
+		System.out.println("Multiplication of Two Numbers: "+(a*b));
+		System.out.println("Division Of two nmbers are: "+(a/b));
+
+	}
+
+}
---
+++ Operators/src/IncrementDecrementOperators.java
@@ -0,0 +1,19 @@
+
+public class IncrementDecrementOperators {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		int result1 = ++a;
+		int result2 = --a;
+		int result3 = a++;
+		int result4 = a--;
+		
+		System.out.println("result1 = "+result1);
+		System.out.println("result2 = "+result2);
+		System.out.println("result3 = "+result3);
+		System.out.println("result4 = "+result4);
+
+	}
+
+}
